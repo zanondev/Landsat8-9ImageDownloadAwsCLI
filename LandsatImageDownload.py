568aca672d969ea9a490ee370246f300d9edb1a9
Lucas Zanon <109632699+zanondev@users.noreply.github.com>
2023-09-13 17:11:52 -0300

    Update LandsatImageDownload.py

--- LandsatImageDownload.py
+++ LandsatImageDownload.py
@@ -2,7 +2,7 @@ import os
 
 # -----------------------------------------------------------
 # ORIENTAÇÕES:
-# - Estar instalado e configurado CLI AWS. (contate um membro do time de tecnologia para auxilio).
+# - Estar instalado e configurado CLI AWS.
 # - Utilizar apenas imagens do Landsat Collection 2 Level-1.
 # - Diretorio: O download sera realizado em uma pasta com o nome da imagem, que será criada dentro do diretorio em que se encontra o script.
 # - Nome da imagem: Certifique-se que a imagem seja Landsat Collection 2 Level-1. Exemplo de input: LC08_L1TP_221080_20201223_20210310_02_T1.
